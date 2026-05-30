e2e9b809ec16022540d1aef6cdc67618f84dc34a
RoeRL <bimo.17168@gmail.com>
2026-05-30 18:23:16 +0700

    route[RR]: Generate set CRUD for MangaController

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MangaController;
 
 Route::get('/', fn() => redirect()->route('manga.index'));
+Route::resource('manga', MangaController::class);
+
